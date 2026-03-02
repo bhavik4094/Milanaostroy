4fc1b32ddee7a200f0adc2562faeb0bf00f07580
Bhavik <bhavik4094@gmail.com>
2026-03-02 19:23:01 +0530

    add new page

--- ResidemHTML/js/custom-marquee.js
+++ ResidemHTML/js/custom-marquee.js
@@ -1,28 +1,28 @@
-$(function(){
-    $('.de-marquee-list').marquee({
-        direction: 'left',
+$(function() {
+    $(".de-marquee-list").marquee({
+        direction: "left",
         duration: 60000,
         gap: 0,
         delayBeforeStart: 0,
         duplicated: true,
-        startVisible: true
+        startVisible: true,
     });
 
-    $('.de-marquee-list-2').marquee({
-        direction: 'right',
+    $(".de-marquee-list-2").marquee({
+        direction: "right",
         duration: 60000,
         gap: 0,
         delayBeforeStart: 0,
         duplicated: true,
-        startVisible: true
+        startVisible: true,
     });
 
-    $('.wm-carousel').marquee({
-        direction: 'left',
+    $(".wm-carousel").marquee({
+        direction: "left",
         duration: 20000,
         gap: 100,
         delayBeforeStart: 0,
         duplicated: true,
-        startVisible: false
+        startVisible: false,
     });
 });
